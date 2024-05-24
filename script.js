939ea0b2fc7c4783864da80d3a2ce0ead1d6083f
NOTHACKERXD <158583681+NOTHACKERXD@users.noreply.github.com>
2024-05-24 22:12:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let attempts = 1;
+let onyes = new Audio('hack.mp3');
+let onstart = new Audio('ephi.mp3');
+onyes.volume = 1;
+
+document.getElementById('yesBtn').addEventListener('click', function () {
+
+    onstart.pause();
+    onyes.play();
+
+    setTimeout(function () {
+        alert('now your IP is mine hehehe ❤️');
+        location.reload(); 
+    }, 3000);
+});
+
+
+const phrases = [
+  
+        "Happy birthday Lydia!",
+        "May your day be filled with laughter, joy, and unforgettable moments.",
+        "Wishing you a day as bright and beautiful as you are.Happy birthday!",
+        "Cheers to another year of wonderful adventures and cherished memories.Happy birthday!",
+        "Sending you smiles, hugs, and lots of love on your special day.Happy birthday!",
+        "Here's to celebrating you and all the amazing things you bring into this world. Happy birthday!",
+        "May your birthday be sprinkled with happiness, wrapped in love, and filled with all your heart desires.Happy birthday!",
+        "Another year older, another reason to celebrate the incredible person you are.Happy birthday!",
+        "On your birthday, may you be surrounded by friends, family, and all the happiness life can offer.Happy birthday!"
+
+
+
+
+
+];
+
+let yesBtn = document.getElementById('yesBtn');
+let noBtn = document.getElementById('noBtn');
+
+document.getElementById('noBtn').addEventListener('click', function () {
+
+/*
+    if (attempts === phrases.indexOf("THA GAMITHEIS FILE")) {
+
+        noBtn.style.backgroundImage = "url('https://i1.prth.gr/images/w880/files/2022-11-10/tik_tok_in__2_.jpg')";
+        noBtn.style.backgroundSize = "100% 100%";
+        noBtn.style.color = "white";
+    } else {
+        noBtn.style.backgroundImage = "none";
+        noBtn.style.color = "initial";
+    }
+ */
+
+    if (attempts < phrases.length - 1) {
+        onstart.play();
+        let newQuestion = phrases[attempts];
+        document.getElementById('question').textContent = newQuestion;
+        //document.getElementById('question').style.fontSize = (16 + attempts) + 'px';
+        attempts++;
+    } else {
+        document.getElementById('question').textContent = phrases[phrases.length - 1];
+        noBtn.style.display = 'none';
+        yesBtn.style.position = 'absolute';
+        yesBtn.style.left = '45%';
+        yesBtn.style.top = '45%';
+        //yesBtn.style.transform = 'translateX(-50%)';
+       // yesBtn.style.fontSize = ;
+    }
+   
+});
